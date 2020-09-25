857099b1991c02e8dd14b861e1759940dddf3613
swastika <sunitamamgai@gmail.com>
2020-09-25 19:23:57 +0530

    demo1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = http.createServer((req,res)=>{
     if (url ==='/'){
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message method="POST><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
